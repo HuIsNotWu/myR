362e369e0bb164cafee11042d9bb3795adb436a7
BunShinn <BunShinn@126.com>
2016-02-21 22:58:03 +0800

    Signed-off-by: BunShinn <BunShinn@126.com>

--- Rcookbook.R
+++ Rcookbook.R
@@ -51,3 +51,21 @@ lst["c3"]
 #列出相应目录下的文件名
 list.files("E:/01 工作/01 信审/201601/20160129/")
 system.file()
+
+x <- 1:9
+x <- seq(1,10,0.1)
+
+x==3
+which(x==3)
+sum(x>3)
+x>3
+rev(x)
+x[!is.na(x)]
+weight = c(150, 135, 210, 140)
+height = c(65, 61, 70, 65)
+gender = c("Fe","Fe","M","Fe")
+study <- data.frame(weight,height,gender)
+names(study) <- c("wei","hei","gen")
+study[["w"]]
+
+unstack(data.frame(a =c("x","y","y","z","x"),b=c(1,2,3,4,5)),b~a)
