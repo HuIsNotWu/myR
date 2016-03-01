113eee75e3b404bc6a6d2e3d773e42bbd9c0cdb2
huisnotwu <412319433@qq.com>
2016-03-01 18:04:13 +0800

    Signed-off-by: huisnotwu <412319433@qq.com>

--- ggplot2.R
+++ ggplot2.R
@@ -26,7 +26,7 @@ tmp <- df$婚姻
 tmp[tmp !="已婚" | tmp !="未婚"]=1
 tmp[tmp =="已婚"]=10
 tmp[tmp =="未婚"]=2
-m <- mean(as.numeric(tmp),na.rm = T)
+m <- round(mean(as.numeric(tmp),na.rm = T))
 tmp[is.na(tmp)]=ifelse(m<5,m,5)
 dfscores$婚姻 <- as.numeric(tmp)
 
@@ -36,7 +36,7 @@ tmp[tmp =="本科"]=9
 tmp[tmp =="中专"]=6
 tmp[tmp =="专科"]=3
 tmp[tmp =="高中及以下"]=1
-m <- mean(as.numeric(tmp),na.rm = T)
+m <- round(mean(as.numeric(tmp),na.rm = T))
 tmp[is.na(tmp)]=ifelse(m<5,m,5)
 dfscores$学历 <- as.numeric(tmp)
 
@@ -45,14 +45,14 @@ tmp[tmp =="高层管理" | tmp =="中层管理"]=10
 tmp[tmp =="企业法人"]=8
 tmp[tmp =="基础管理" | tmp =="一般员工"]=6
 tmp[tmp =="其他"]=1
-m <- mean(as.numeric(tmp),na.rm = T)
+m <- round(mean(as.numeric(tmp),na.rm = T))
 tmp[is.na(tmp)]=ifelse(m<5,m,5)
 dfscores$岗位类型 <- as.numeric(tmp)
 
 tmp <- df$户籍是否为居住地本地
 tmp[tmp =="是"]=10
 tmp[tmp =="否"]=2
-m <- mean(as.numeric(tmp),na.rm = T)
+m <- round(mean(as.numeric(tmp),na.rm = T))
 tmp[is.na(tmp)]=ifelse(m<5,m,5)
 dfscores$户籍是否为居住地本地 <- as.numeric(tmp)
 
@@ -60,35 +60,35 @@ tmp <- t <- df$客户类型
 tmp[t !="受薪" & t !="私营业主"]=2
 tmp[t =="受薪"]=6
 tmp[t =="私营业主"]=10
-m <- mean(as.numeric(tmp),na.rm = T)
+m <- round(mean(as.numeric(tmp),na.rm = T))
 tmp[is.na(tmp)]=ifelse(m<5,m,5)
 dfscores$客户类型 <- as.numeric(tmp)
 
 tmp <- t <- df$客户类型
 tmp[t =="私营业主"]=10
 tmp[t !="私营业主"]=3
-m <- mean(as.numeric(tmp),na.rm = T)
+m <- round(mean(as.numeric(tmp),na.rm = T))
 tmp[is.na(tmp)]=ifelse(m<5,m,5)
 dfscores$是否私营业主 <- as.numeric(tmp)
 
 tmp <- df$房产情况
 tmp[tmp =="有"]=6
 tmp[tmp =="无"]=4
-m <- mean(as.numeric(tmp),na.rm = T)
+m <- round(mean(as.numeric(tmp),na.rm = T))
 tmp[is.na(tmp)]=ifelse(m<5,m,5)
 dfscores$房产情况 <- as.numeric(tmp)
 
 tmp <- df$按揭情况
 tmp[tmp =="有"]=10
 tmp[tmp =="无"]=2
-m <- mean(as.numeric(tmp),na.rm = T)
+m <- round(mean(as.numeric(tmp),na.rm = T))
 tmp[is.na(tmp)]=ifelse(m<5,m,5)
 dfscores$按揭情况 <- as.numeric(tmp)
 
 tmp <- df$车辆情况
 tmp[tmp =="有"]=10
 tmp[tmp =="无"]=1
-m <- mean(as.numeric(tmp),na.rm = T)
+m <- round(mean(as.numeric(tmp),na.rm = T))
 tmp[is.na(tmp)]=ifelse(m<5,m,5)
 dfscores$车辆情况 <- as.numeric(tmp)
 
@@ -96,7 +96,7 @@ tmp <- df$发薪方式
 tmp[tmp =="网银"]=10
 tmp[tmp =="网银+现金"]=6
 tmp[tmp =="现金"]=2
-m <- mean(as.numeric(tmp),na.rm = T)
+m <- round(mean(as.numeric(tmp),na.rm = T))
 tmp[is.na(tmp)]=ifelse(m<5,m,5)
 dfscores$发薪方式 <- as.numeric(tmp)
 
@@ -109,7 +109,7 @@ tmp[tmp >=20000 & tmp < 30000]=10
 tmp[tmp >=30000 & tmp < 50000]=9
 tmp[tmp >=50000 & tmp < 100000]=8
 tmp[tmp >=100000]=8
-m <- mean(as.numeric(tmp),na.rm = T)
+m <- round(mean(as.numeric(tmp),na.rm = T))
 tmp[is.na(tmp)]=ifelse(m<5,m,5)
 dfscores$核实收入 <- as.numeric(tmp)
 
@@ -121,7 +121,7 @@ tmp[tmp>500 & tmp<=1000]=10
 tmp[tmp>1000 & tmp<=2000]=9
 tmp[tmp>2000 & tmp<=5000]=6
 tmp[tmp>5000]=2
-m <- mean(as.numeric(tmp),na.rm = T)
+m <- round(mean(as.numeric(tmp),na.rm = T))
 tmp[is.na(tmp)]=ifelse(m<5,m,5)
 dfscores$信用卡负债 <- as.numeric(tmp)
 
@@ -132,7 +132,7 @@ tmp[tmp>1500 & tmp<=3000]=7
 tmp[tmp>3000 & tmp<=5000]=6
 tmp[tmp>5000 & tmp<=10000]=4
 tmp[tmp>10000]=2
-m <- mean(as.numeric(tmp),na.rm = T)
+m <- round(mean(as.numeric(tmp),na.rm = T))
 tmp[is.na(tmp)]=ifelse(m<5,m,5)
 dfscores$信用贷款负债 <- as.numeric(tmp)
 
@@ -147,7 +147,7 @@ tmp <- 10-tmp
 # tmp[tmp==5]=4
 # tmp[tmp==6]=3
 # tmp[tmp>=7]=2
-m <- mean(as.numeric(tmp),na.rm = T)
+m <- round(mean(as.numeric(tmp),na.rm = T))
 tmp[is.na(tmp)]=ifelse(m<5,m,5)
 dfscores$人行近3个月查询次数 <- tmp
 
@@ -157,7 +157,7 @@ tmp[t>0 & t<=100000]=8
 tmp[t>100000 & t<=200000]=6
 tmp[t>200000 & t<=500000]=4
 tmp[t>500000]=3
-m <- mean(as.numeric(tmp),na.rm = T)
+m <- round(mean(as.numeric(tmp),na.rm = T))
 tmp[is.na(tmp)]=ifelse(m<5,m,5)
 dfscores$人行房贷总金额 <- as.numeric(tmp)
 
@@ -168,7 +168,7 @@ tmp[t>5000 & t<=10000]=6
 tmp[t>10000 & t<=20000]=8
 tmp[t>20000 & t<=50000]=10
 tmp[t>50000]=8
-m <- mean(as.numeric(tmp),na.rm = T)
+m <- round(mean(as.numeric(tmp),na.rm = T))
 tmp[is.na(tmp)]=ifelse(m<5,m,5)
 dfscores$人行单张信用卡最大额度 <- as.numeric(tmp)
 
@@ -178,7 +178,7 @@ tmp[t==1]=9
 tmp[t==2]=8
 tmp[t %in% c(3,4)]=10
 tmp[t>=5]=4
-m <- mean(as.numeric(tmp),na.rm = T)
+m <- round(mean(as.numeric(tmp),na.rm = T))
 tmp[is.na(tmp)]=ifelse(m<5,m,5)
 dfscores$人行正常状态信用卡张数 <- as.numeric(tmp)
 
@@ -187,16 +187,16 @@ tmp[t==0]=10
 tmp[t==1]=8
 tmp[t %in% c(2,3)]=6
 tmp[t>=4]=2
-m <- mean(as.numeric(tmp),na.rm = T)
+m <- round(mean(as.numeric(tmp),na.rm = T))
 tmp[is.na(tmp)]=ifelse(m<5,m,5)
 dfscores$人行近12个月的逾期次数 <- as.numeric(tmp)
 
 tmp <- t <-  df$人行近3个月的逾期次数
 tmp[t==0]=10
 tmp[t==1]=7
-tmp[t==2]=5
-tmp[t>=3]=3
-m <- mean(as.numeric(tmp),na.rm = T)
+tmp[t==2]=3
+tmp[t>=3]=1
+m <- round(mean(as.numeric(tmp),na.rm = T))
 tmp[is.na(tmp)]=ifelse(m<5,m,5)
 dfscores$人行近3个月的逾期次数 <- as.numeric(tmp)
 
@@ -204,7 +204,7 @@ dfscores$人行近3个月的逾期次数 <- as.numeric(tmp)
 # tmp[t>0 & t<0.5]=1
 # tmp[t>=0.5 & t<1]=0
 # tmp[t>=1]=-2
-# m <- mean(as.numeric(tmp),na.rm = T)
+# m <- round(mean(as.numeric(tmp),na.rm = T))
 # tmp[is.na(t) | is.nan(t)] =ifelse(m<5,m,5)
 # dfscores$近3逾占12比 <- as.numeric(tmp)
 
@@ -215,7 +215,7 @@ tmp[t>0.25 & t <=0.5]=8
 tmp[t>0.5 & t <=0.8]=7
 tmp[t>0.8 & t <=1]=6
 tmp[t>1]=1
-m <- mean(as.numeric(tmp),na.rm = T)
+m <- round(mean(as.numeric(tmp),na.rm = T))
 tmp[is.na(tmp)]=ifelse(m<5,m,5)
 dfscores$贷记卡最高使用率 <- as.numeric(tmp)
 
@@ -228,7 +228,7 @@ tmp[t>12 & t <=24]=6
 tmp[t>24 & t <=36]=4
 tmp[t>36 & t <=60]=8
 tmp[t>60]=10
-m <- mean(as.numeric(tmp),na.rm = T)
+m <- round(mean(as.numeric(tmp),na.rm = T))
 tmp[is.na(tmp)]=ifelse(m<5,m,5)
 dfscores$最早一张贷记卡开户距离申请月月数 <- as.numeric(tmp)
 
@@ -239,21 +239,21 @@ tmp[t==2]=10
 tmp[t>2 & t <=4]=8
 tmp[t>4 & t <=10]=9
 tmp[t>10 ]=7
-m <- mean(as.numeric(tmp),na.rm = T)
+m <- round(mean(as.numeric(tmp),na.rm = T))
 tmp[is.na(tmp)]=ifelse(m<5,m,5)
 dfscores$人行贷款次数 <- as.numeric(tmp)
 
-tmp <- t <-  df$负债率
+tmp <- t <-  df$外部负债率
 tmp[t==0]=10
-tmp[t>0 & t <=0.1]=9
-tmp[t>0.1 & t <=0.3]=8
-tmp[t>0.3 & t <=0.6]=7
-tmp[t>0.6 & t <=0.9]=6
+tmp[t>0 & t <=0.1]=8
+tmp[t>0.1 & t <=0.3]=6
+tmp[t>0.3 & t <=0.6]=5
+tmp[t>0.6 & t <=0.9]=7
 tmp[t>0.9 & t <=2]=5
 tmp[t>2]=1
-m <- mean(as.numeric(tmp),na.rm = T)
+m <- round(mean(as.numeric(tmp),na.rm = T))
 tmp[is.na(tmp)]=ifelse(m<5,m,5)
-dfscores$负债率 <- as.numeric(tmp)
+dfscores$外部负债率 <- as.numeric(tmp)
 
 tmp <- t <- as.numeric(gsub("*个月","",df$车辆使用时间))
 tmp[t>0 & t<=3] =2
@@ -261,9 +261,9 @@ tmp[t>3 & t<=6] =5
 tmp[t>6 & t<=12] =10
 tmp[t>12 & t<=24] =8
 tmp[t>24] =7
-m <- mean(as.numeric(tmp),na.rm = T)
+m <- round(mean(as.numeric(tmp),na.rm = T))
 # tmp[is.na(tmp)]=ifelse(m<5,m,5)
-tmp[is.na(t)]=mean(as.numeric(tmp),na.rm = T)
+tmp[is.na(t)]=7
 dfscores$车辆购买时长 <- as.numeric(tmp)
 
 tmp <- t <-  df$负债率
@@ -293,7 +293,7 @@ score <-
   
   55*信用卡负债+
   55*信用贷款负债+
-  100*人行近3个月查询次数+
+  110*人行近3个月查询次数+
   20*人行房贷总金额+
   60*人行单张信用卡最大额度+
   30*人行正常状态信用卡张数+
@@ -302,7 +302,7 @@ score <-
   70*贷记卡最高使用率+
   25*最早一张贷记卡开户距离申请月月数+
   25*人行贷款次数+
-  50*负债率+
+  40*外部负债率+
   
   50*车辆购买时长+
   50*车龄
